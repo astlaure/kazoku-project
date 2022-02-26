98a573d70e232c3e329a66d76c8e8643590d9b73
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2022-02-25 21:21:34 -0500

    created angular and spring api modules ✨

---
+++ kazoku-api/src/main/java/org/astlaure/kazoku/KazokuApiApplication.java
@@ -0,0 +1,13 @@
+package org.astlaure.kazoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazokuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KazokuApiApplication.class, args);
+    }
+
+}
---
+++ kazoku-api/src/test/java/org/astlaure/kazoku/KazokuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.astlaure.kazoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazokuApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
